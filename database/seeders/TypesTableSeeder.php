6213a9a62e87de000e17bb97e782fb8ea3989c07
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-10 15:40:05 +0200

    add seedeer po types

--- database/seeders/TypesTableSeeder.php
+++ database/seeders/TypesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,25 @@ class TypesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = [
+            [
+                'name' => 'Full Stack',
+                'description' => 'Tipo di sviluppo web che copre il Front End e il Back End.',
+            ],
+
+            [
+                'name' => 'Front End',
+                'description' => 'Tipo di sviluppo che copre solo il Front End, ovvero la parte visibile all\'utente.',
+            ],
+
+            [
+                'name' => 'Back End',
+                'description' => 'Tipo di sviluppo che copre solo il Back End, ovvero la parte non visibile all\'utente.',
+            ],
+        ];
+
+        foreach ($types as $type) {
+            Type::create($type);
+        }
     }
 }
