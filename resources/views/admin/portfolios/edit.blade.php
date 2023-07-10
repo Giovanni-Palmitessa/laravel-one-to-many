089e6f8bf94cc977d14a08e6b69e7782b21069aa
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-10 16:30:56 +0200

    add form dor select in edit.blade

--- resources/views/admin/portfolios/edit.blade.php
+++ resources/views/admin/portfolios/edit.blade.php
@@ -40,6 +40,23 @@
         </div>
 
         <div class="mb-3">
+            <label for="category" class="form-label">Type</label>
+
+            <select class="form-select @error('type_id') is-invalid @enderror" aria-label="Default select example" id="type" name="type_id">    
+                @foreach ($types as $type)
+                    <option value="{{$type->id}}" @if (old('type_id', $portfolio->type->id) == $type->id)
+                        selected
+                    @endif >{{$type->name}}</option>
+                @endforeach
+            </select>
+            @error('type_id')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
             <label for="url_image" class="form-label">Immagine</label>
             <input
                 type="text"
