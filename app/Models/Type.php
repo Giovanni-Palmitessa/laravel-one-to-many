0839e4c2efd163d4e51a31a53dcf5c014dbe1526
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-10 16:00:42 +0200

    add method portfolios in type model

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Portfolio;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Type extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    public function portfolios()
+    {
+        return $this->hasMany(Portfolio::class);
+    }
 }
