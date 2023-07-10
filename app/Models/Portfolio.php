cf2275b1cec0cd392787cbac8c7beed939a8fa64
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-10 15:58:47 +0200

    add method type in portfolio model

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Type;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Portfolio extends Model
 {
     use HasFactory;
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
