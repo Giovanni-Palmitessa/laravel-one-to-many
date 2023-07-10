8715543e2c7e48570b30e57c93114fc77884c036
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-10 15:25:26 +0200

    add migration for types table

--- database/migrations/2023_07_10_131528_create_types_table.php
+++ database/migrations/2023_07_10_131528_create_types_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('description', 200);
         });
     }
 
